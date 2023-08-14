68ca341686688ae755d9be9a4d840c80d8acaac6
Bekzod <122161475+rbekzod@users.noreply.github.com>
2023-08-14 14:32:13 +0500

    Added filter and search to service model (#1)

--- service/views.py
+++ service/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 from .serializers import ServiceTypeSerializer, ServiceSerializer
 from .models import ServiceType, Service
 
@@ -11,3 +13,6 @@ class ServiceTypeViewSet(viewsets.ModelViewSet):
 class ServiceViewSet(viewsets.ModelViewSet):
     queryset = Service.objects.all()
     serializer_class = ServiceSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['region', 'district', 'price', 'people_count']
+    search_fields = ['title']
